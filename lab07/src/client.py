ad028de10e2c3b5a01035adde887e48b185ccdff
Mell0r <malovtarasov@gmail.com>
2024-05-26 15:41:00 +0300

    feat: 3rd task is done (client show stats now)

--- lab07/src/client.py
+++ lab07/src/client.py
@@ -8,6 +8,9 @@ client.settimeout(1.0)
 
 server_address = ("127.0.0.1", 41234)
 
+min_rtt: float | None = None
+max_rtt: float | None = None
+sum_rtt: float = 0
 packets_lost = 0
 
 for i in range(1, 11):
@@ -18,10 +21,19 @@ for i in range(1, 11):
     try:
         response, address = client.recvfrom(1024)
         rtt = (datetime.now() - sent_time).total_seconds()
+
+        min_rtt = rtt if min_rtt is None else min(min_rtt, rtt)
+        max_rtt = rtt if max_rtt is None else max(max_rtt, rtt)
+        sum_rtt += rtt
+
         print(f"Got response: '{response.decode()}' in time {rtt}")
+        print(f"Min rtt: {min_rtt}")
+        print(f"Max rtt: {max_rtt}")
+        print(f"Average rtt: {sum_rtt / i}")
 
     except socket.timeout:
         packets_lost += 1
         print("Request timed out")
 
+print(f"Package loss: {packets_lost / 10 * 100}%")
 client.close()
